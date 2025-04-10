0fd72749ac5da47b4ea33353a6db9b64104f0e5d
xsynch <jdstrickland@gmail.com>
2025-04-09 22:01:19 -0400

    updated parserequestline function to return a requestline pointer

--- internal/request/request.go
+++ internal/request/request.go
@@ -22,42 +22,41 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 	if err != nil {
 		return nil, err
 	}
-	httpVersion, requestTarget,method,err := parseRequestLine(string(b))
+	res,err := parseRequestLine(string(b))
 	if err != nil {
 		return nil,err 
 	}
 
-	requestLine := RequestLine{
-		HttpVersion: httpVersion,
-		RequestTarget: requestTarget,
-		Method: method,
-	}
+
 	
 
 	return &Request{
-		requestLine,
+		*res,
 	},nil 
 }
 
-func parseRequestLine(data string) (string,string,string,error){
+func parseRequestLine(data string) (*RequestLine,error){
 	
 	lines := strings.Split(data,"\r\n")
 	
 	requestLine := strings.Split(lines[0]," ")
 	if len(requestLine) != 3 {
-		return "","","",fmt.Errorf("invalid request line: %s",requestLine)
+		return nil,fmt.Errorf("invalid request line: %s",requestLine)
 	}
 	
 	if !regexp.MustCompile(`^[A-Z]*$`).MatchString(requestLine[0]) {
-		return "","","",fmt.Errorf("invalid method: %s",requestLine[0])
+		return nil,fmt.Errorf("invalid method: %s",requestLine[0])
 		
 	}
 	if strings.Compare(requestLine[2],"HTTP/1.1") != 0 {
-		return "","","",fmt.Errorf("invalid http version: %s",requestLine[2])
+		return nil,fmt.Errorf("invalid http version: %s",requestLine[2])
 	}
 	method := requestLine[0]
 	targetPath := requestLine[1]
 	httpVersion := "1.1"
 	
-	return httpVersion,targetPath,method,nil 
+	return &RequestLine{
+		HttpVersion: httpVersion,
+		RequestTarget: targetPath,
+		Method: method,},nil 
 }
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -57,7 +57,32 @@ require.Error(t, err)
 	_, err = RequestFromReader(strings.NewReader("OPTIONS /prime/rib TCP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
 	require.Error(t, err)
 	
+
+	// Test: Good GET Request line
+	reader := &chunkReader{
+	data:            "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+	numBytesPerRead: 3,
+	}
 	
+	r, err = RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good GET Request line with path
+	reader = &chunkReader{
+		data:            "GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+		numBytesPerRead: 1,
+	}
+	r, err = RequestFromReader(reader)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+		
 }
 
 
